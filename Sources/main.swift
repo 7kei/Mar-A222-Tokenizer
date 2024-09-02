4fa492e2540f0c23f9c8b530b5a4710a92ca43f4
Keian Mar <150799485+7kei@users.noreply.github.com>
2024-09-02 23:36:33 +0800

    Implement initial tokenizer function

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,14 +1,37 @@
+import Foundation
+
 enum TokenType {
     case unspecified
 }
 
 struct Token {
     let type: TokenType
-    let token: String
+    let tokenString: String
 }
 
-func tokenize(_ input: String) -> () {
+func tokenize(_ input: String) -> [Token] {
+    let delimiter: String = "@"
 
+    var tokens: [Token] = []
+    let splitString: [String] = input.components(separatedBy: delimiter)
+
+    for split: String in splitString {
+        var type: TokenType = .unspecified
+        
+        // if empty
+        if split == " " || split == "" {
+            type = .unspecified
+        }
+
+        tokens.append(Token(type: type, tokenString: split))
+    }
+
+    return tokens
 }
 
 let tokens: [Token] = tokenize("Hello@World!")
+
+// Print all tokens
+for token in tokens {
+    print("Type: \(token.type) - Token: \(token.tokenString)")
+}
